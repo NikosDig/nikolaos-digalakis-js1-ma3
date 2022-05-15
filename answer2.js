685002769b5d160a69f623906fa338f4a75d6c8f
NikosDigalakis <nikos_23_chania@hotmail.com>
2022-05-15 20:13:07 +0200

    Update answer2.js

--- answer2.js
+++ answer2.js
@@ -4,15 +4,14 @@ const key = "13cc7e028dcf4a7d9d4ae4004d1aa685";
 const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
 const answer = document.querySelector(".answers");
 
-
 async function getData() {
   try {
     const response = await fetch(url);
     const data = await response.json();
     const result = data.results;
-    console.log(result);
-    console.log(data.results.length);
-    console.log(data.results[2].tags.length);
+    // console.log(result);
+    // console.log(data.results.length);
+    // console.log(data.results[2].tags.length);
     answer.innerHTML = "";
     for (i = 0; i < result.length; i++) {
       console.log(i.rating);
